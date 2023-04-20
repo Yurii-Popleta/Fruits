cfa85a436d5ce090053b15acddf8b2f9abad9242
Yurii Popleta <yurka279@gmail.com>
2023-04-20 19:01:22 +0200

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Admin on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
